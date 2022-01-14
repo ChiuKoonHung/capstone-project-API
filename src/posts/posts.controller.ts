13c2db97e52d7df69e2b77012874f1aeddf37d9c
ChiuKoonHung <chiukoonhung1991@gmail.com>
2022-01-14 11:11:59 +0800

    CRUD setup completed

---
+++ src/posts/posts.controller.ts
@@ -0,0 +1,31 @@
+import { Controller } from '@nestjs/common';
+import { PostsService } from './posts.service';
+import { PostModel } from './posts.interface';
+import { Get, Param, ParseIntPipe, Post, Body, Put } from '@nestjs/common';
+
+@Controller('posts')
+export class PostsController {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  public findAll(): Array<PostModel> {
+    return this.postsService.findAll();
+  }
+
+  @Get(':id')
+  public findOne(@Param('id', ParseIntPipe) id: number): PostModel {
+    //param is string type. id is number. use ParseIntPipe to convert id to number.
+    return this.postsService.findOne(id);
+  }
+
+  @Post()
+  public create(@Body() post: PostModel): PostModel {
+    return this.postsService.create(post);
+  }
+
+  @Put(':id')
+  public update(@Param('id', ParseIntPipe) id: number, @Body() post: PostModel,): PostModel {
+    return this.postsService.update(id, post);
+  }
+}
+
---
+++ src/posts/posts.service.ts
@@ -0,0 +1,75 @@
+import { Injectable, Logger, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
+import { PostModel } from './posts.interface';
+
+@Injectable()
+export class PostsService {
+  private posts: Array<PostModel> = [];
+
+  public findAll(): Array<PostModel> {
+    Logger.log('Returning all posts');
+    return this.posts;
+  }
+
+  public findOne(id: number): PostModel {
+    const post: PostModel = this.posts.find((post) => post.id === id);
+
+    if (!post) {
+      throw new NotFoundException('Post not found.');
+    }
+
+    return post;
+  }
+
+  public create(post: PostModel): PostModel {
+    Logger.log(`Creating post with title: ${post.title}`);
+
+    // if the title is already in use by another post
+    const titleExists: boolean = this.posts.some(
+      (item) => item.title === post.title,
+    );
+    if (titleExists) {
+      throw new UnprocessableEntityException('Post title already exists.');
+    }
+
+    // find the next id for a new blog post
+    const maxId: number = Math.max(...this.posts.map((post) => post.id), 0);
+    const id: number = maxId + 1;
+
+    const blogPost: PostModel = {
+      ...post,
+      id,
+    };
+
+    this.posts.push(blogPost);
+
+    return blogPost;
+  }
+
+  public update(id: number, post: PostModel): PostModel {
+    Logger.log(`Updating post with id: ${id}`);
+
+    const index: number = this.posts.findIndex((post) => post.id === id);
+
+    // -1 is returned when no findIndex() match is found
+    if (index === -1) {
+      throw new NotFoundException('Post not found.');
+    }
+
+    // if the title is already in use by another post
+    const titleExists: boolean = this.posts.some(
+      (item) => item.title === post.title && item.id !== id,
+    );
+    if (titleExists) {
+      throw new UnprocessableEntityException('Post title already exists.');
+    }
+
+    const blogPost: PostModel = {
+      ...post,
+      id,
+    };
+
+    this.posts[index] = blogPost;
+
+    return blogPost;
+  }
+}
